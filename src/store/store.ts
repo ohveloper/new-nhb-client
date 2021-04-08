5c9ac14a0e847c2406e1d355c457237593aa3183
riley909 <increaseblue666@gmail.com>
2021-04-08 21:46:35 +0900

    Feature#7: settings

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,3 +5,4 @@ import rootReducer from '../reducers/index';
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
+export type RootReducerType = ReturnType<typeof rootReducer>;
