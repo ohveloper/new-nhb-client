bc077c761871167ee72465817a7a12c10472acc6
riley909 <increaseblue666@gmail.com>
2021-04-08 21:47:46 +0900

    Feature#7: poemActions

--- src/actions/poemActions.ts
+++ src/actions/poemActions.ts
@@ -1,17 +1,35 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';
+import {
+  poemDispatchType,
+  CREATE_POEM,
+  MODIFY_POEM,
+  REMOVE_POEM,
+} from './actionTypes';
 
-export const CREATE_POEM = 'CREATE_POEM';
-export const MODIFY_POEM = 'MODIFY_POEM';
-export const REMOVE_POEM = 'REMOVE_POEM';
-
-const FEED_URL = 'http://localhost:3000/feed';
+const FEED_URL = 'http://54.173.92.145:5000/feed';
 
 //? -------------------액션 생성 함수 ----------------------//
-export function createPoem(content: string, word: string) {
-  return async () => {
-    const data = await axios
-      .post(FEED_URL, { content, word })
-      .then((res) => res.data)
-      .catch((err) => console.log(err));
-  };
-}
+// export function createPoem(content: string, word: string) {
+//   return async () => {
+//     const data = await axios
+//       .post(FEED_URL, { content, word })
+//       .then((res) => res.data)
+//       .catch((err) => console.log(err));
+//   };
+// }
+export const createPoem = (content: string, word: string) => async (
+  dispatch: Dispatch<poemDispatchType>
+) => {
+  try {
+    const result = await axios.post(FEED_URL, { content, word });
+    const data = result.data;
+
+    dispatch({
+      type: CREATE_POEM,
+      payload: data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
