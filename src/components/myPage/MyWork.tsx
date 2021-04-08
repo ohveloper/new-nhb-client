b92069ce8472c669f901858213876aff96fec493
ohveloper <ohveloper@gmail.com>
2021-04-08 14:31:36 +0900

    feat19 redux

--- src/components/myPage/MyWork.tsx
+++ src/components/myPage/MyWork.tsx
@@ -1,10 +1,15 @@
 import { RootState } from '../../modules';
 import { PrivateFeed } from '../../modules/getInfo';
 
+interface MyWorkProps {
+  x: string;
+  privateFeeds: PrivateFeed;
+}
+
 export default function MyWork({ x, privateFeeds }: any) {
   return (
     <div>
-      <h1>MyWork</h1>
+      <h1>MyWork Redux</h1>
       <div>유저 네임 : {privateFeeds[x].user.nickName}</div>
       <div>착용중인 뱃지 이름 : {privateFeeds[x].user.tag}</div>
       <div>작성한시 : {privateFeeds[x].content}</div>
