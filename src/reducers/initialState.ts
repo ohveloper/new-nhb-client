1f6c7fe3fbaade1461a6cd3ba5ee85c112567881
riley909 <increaseblue666@gmail.com>
2021-04-10 00:00:28 +0900

    Feature#7: poemReducer

--- src/reducers/initialState.ts
+++ src/reducers/initialState.ts
@@ -1,42 +1,123 @@
+export interface initialState {
+  userInfo: UserInfo;
+  privateFeeds: PrivateFeed[];
+  rank: Rank[];
+  comments: Comment[];
+  userFeeds: UserFeed[];
+  newFeed: newFeed;
+  todaysWord: string[];
+}
+
+export interface Comment {
+  user: CommentUser;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CommentUser {
+  nickName: string;
+}
+
+export interface PrivateFeed {
+  feedId: number;
+  user: PrivateFeedUser;
+  topic: string;
+  content: string[];
+  likes: number;
+  comments: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PrivateFeedUser {
+  nickName: string;
+  tag: string;
+}
+
+export interface Rank {
+  userId: number;
+  nickName: string;
+  like: number;
+  tag: string;
+}
+
+export interface UserFeed {
+  feedId: number;
+  user: PrivateFeedUser;
+  topic: string;
+  content: string[];
+  likes: number;
+  comments: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface UserInfo {
+  nickName: string;
+  introduction: string;
+  tags: { [key: string]: Hund };
+  avatarUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Tags {
+  hund: Hund;
+  pig: Hund;
+  newbie: Hund;
+}
+
+export interface Hund {
+  description: string;
+  isUsed: boolean;
+}
+
+export interface newFeed {
+  content: string[];
+}
+
 export const initialState = {
   userInfo: {
     nickName: '불광동핵주먹',
     introduction: '원펀치 쓰리강냉이',
     tags: {
       hund: { description: '100일연속 글쓰기', isUsed: true },
-      pig: { description: '별 많이받음', isUsed: false },
+      king: { description: '별 많이받음', isUsed: false },
       newbie: { description: '글 처음씀', isUsed: false },
     },
     avatarUrl: 'url',
     createdAt: 'date',
     updatedAt: 'date',
   },
-  privateFeeds: {
-    1: {
+  privateFeeds: [
+    {
+      feedId: 1,
       user: { nickName: '진라면은순한맛', tag: 'tagName' },
-      topic: 'keyword',
-      content: 'content',
+      topic: '주말',
+      content: ['주린배를 부여잡고', '말고기'],
       likes: 23,
       comments: 43,
       createdAt: 'date',
       updatedAt: 'date',
     },
-    2: {
+    {
+      feedId: 2,
       user: { nickName: '아닌데매운맛인데', tag: 'tagName' },
-      topic: 'keyword',
-      content: 'content',
+      topic: '주말',
+      content: ['주단태가 말했습니다', '말고기'],
       likes: 22,
       comments: 1,
       createdAt: 'date',
       updatedAt: 'date',
     },
-  },
+  ],
   rank: [
-    { nickName: 'tester1', like: 123, tag: 'tag' },
-    { nickName: 'tester2', like: 23, tag: 'tag' },
-    { nickName: '크림진짬뽕', like: 3212, tag: 'tag' },
-    { nickName: '굴진짬뽕', like: 123, tag: 'tag' },
-    { nickName: '주님또한명갑니다', like: 123, tag: 'tag' },
+    { userId: 1, nickName: '주님또한명갑니다', like: 12, tag: 'tag' },
+    { userId: 2, nickName: 'tester2', like: 23, tag: 'tag' },
+    { userId: 3, nickName: 'tester1', like: 123, tag: 'tag' },
+    { userId: 4, nickName: '굴진짬뽕', like: 222, tag: 'tag' },
+    { userId: 5, nickName: '크림진짬뽕', like: 3212, tag: 'tag' },
   ],
   comments: [
     {
@@ -58,8 +139,9 @@ export const initialState = {
       updatedAt: 'date',
     },
   ],
-  userFeeds: {
-    3: {
+  userFeeds: [
+    {
+      feedId: 3,
       user: { nickName: '이렇게하는거맞아요?', tag: 'newbie' },
       topic: '시장',
       content: ['시장에 갔습니다', '장군'],
@@ -68,5 +150,19 @@ export const initialState = {
       createdAt: 'date',
       updatedAt: 'date',
     },
+    {
+      feedId: 4,
+      user: { nickName: '타입스크립트', tag: 'newbie' },
+      topic: '시장',
+      content: ['시장에 갔습니다', '장발장'],
+      likes: 32,
+      comments: 354,
+      createdAt: 'date',
+      updatedAt: 'date',
+    },
+  ],
+  newFeed: {
+    content: [],
   },
+  todaysWord: ['시', '장'],
 };
--- src/reducers/poemReducer.ts
+++ src/reducers/poemReducer.ts
@@ -1,34 +1,56 @@
-import {
-  CREATE_POEM,
-  MODIFY_POEM,
-  REMOVE_POEM,
-  poemType,
-  poemDispatchType,
-} from '../actions/actionTypes';
-// import { initialState } from './initialState';
+import { type } from 'node:os';
+import { CREATE_POEM, MODIFY_POEM, REMOVE_POEM } from '../actions/actionTypes';
+import { PoemAction } from '../actions/poemActions';
+import { initialState } from './initialState';
 
-interface initialState {
-  feed?: poemType;
-}
+// interface initialState {
+//   feed?: poemType;
+// }
 
-const initialState: initialState = {};
+// const initialState: initialState = {};
 
-const poemReducer = (
-  state = initialState,
-  action: poemDispatchType
-): initialState => {
+// const poemReducer = (
+//   state = initialState,
+//   action: poemDispatchType
+// ): initialState => {
+//   switch (action.type) {
+//     case CREATE_POEM:
+//       // eslint-disable-next-line no-case-declarations
+//       const { data } = action.payload;
+//       return {
+//         ...state,
+//         feed: { data },
+//       };
+//     case MODIFY_POEM:
+//     case REMOVE_POEM:
+//     default:
+//       return state;
+//       break;
+//   }
+// };
+
+//?--------------------WITH DUMMY DATA---------------------//
+// 상태에서 사용할 데이터 타입
+// export type Poem = {
+//   id: number;
+//   content: string;
+//   word: string;
+// };
+
+function poemReducer(
+  state: initialState = initialState,
+  action: PoemAction
+): initialState {
   switch (action.type) {
     case CREATE_POEM:
-      const { data } = action.payload;
       return {
         ...state,
-        feed: { data },
+        newFeed: action.payload.newFeed,
       };
-    case MODIFY_POEM:
-    case REMOVE_POEM:
+
     default:
       return state;
-      break;
   }
-};
+}
+
 export default poemReducer;
