693a36d73f5d0cd8232711f722205490b26e7057
riley909 <increaseblue666@gmail.com>
2021-04-09 11:34:02 +0900

    Feature#7: route poemInput page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import GetUserInfo from './components/getUserInfo';
 import MyPage from './pages/MyPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <>
-      <div>
-        <div className="App">:sunglasses: </div>
-        <GetUserInfo />
-        <MyPage />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact={true} path="/">
+            <div className="App">:sunglasses: </div>
+          </Route>
+          <Route path="/main">
+            <MainPage />
+          </Route>
+          <Route path="/mypage">
+            <GetUserInfo />
+            <MyPage />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
