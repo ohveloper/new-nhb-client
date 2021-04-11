2eebe0051153f2000c3e2e3d8d7260593676b61b
ohveloper <ohveloper@gmail.com>
2021-04-11 21:49:47 +0900

    temp

--- src/actions/getInfoActions.ts
+++ src/actions/getInfoActions.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from 'axios';
+
 import {
   PrivateFeed,
   UserInfo,
@@ -12,35 +14,63 @@ export const GET_RANK = 'GET_RANK' as const;
 export const GET_COMMENTS = 'GET_COMMENTS' as const;
 export const GET_USER_FEEDS = 'GET_USER_FEEDS' as const;
 
-export const myPageActions = {
-  getUserInfo: (userInfo: UserInfo) => ({
+// const apiClient = axios.create({
+//   baseURL: 'https://localhost:4000',
+//   responseType: 'json',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// export const getUserInfo = async () => {
+//   try {
+//     const response = await apiClient.get<UserInfo>('/user');
+//     const userInfo = response.data;
+
+//     return {
+//       type: GET_USER_INFO,
+//       payload: {
+//         userInfo,
+//       },
+//     };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+export const getUserInfo = (userInfo: UserInfo) => {
+  return {
     type: GET_USER_INFO,
     payload: {
       userInfo,
     },
-  }),
-  getPrivateFeeds: (privateFeeds: PrivateFeed[]) => ({
-    type: GET_PRIVATE_FEEDS,
-    payload: {
-      privateFeeds,
-    },
-  }),
-  getRank: (rank: Rank[]) => ({
-    type: GET_RANK,
-    payload: {
-      rank,
-    },
-  }),
-  getComments: (comments: Comment[]) => ({
-    type: GET_COMMENTS,
-    payload: {
-      comments,
-    },
-  }),
-  getUserFeeds: (userFeeds: UserFeed[]) => ({
-    type: GET_USER_FEEDS,
-    payload: {
-      userFeeds,
-    },
-  }),
+  };
 };
+
+export const getPrivateFeeds = (privateFeeds: PrivateFeed[]) => ({
+  type: GET_PRIVATE_FEEDS,
+  payload: {
+    privateFeeds,
+  },
+});
+
+export const getRank = (rank: Rank[]) => ({
+  type: GET_RANK,
+  payload: {
+    rank,
+  },
+});
+
+export const getComments = (comments: Comment[]) => ({
+  type: GET_COMMENTS,
+  payload: {
+    comments,
+  },
+});
+
+export const getUserFeeds = (userFeeds: UserFeed[]) => ({
+  type: GET_USER_FEEDS,
+  payload: {
+    userFeeds,
+  },
+});
