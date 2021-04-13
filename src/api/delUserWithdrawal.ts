b1faf7d23ab6dc94488e7b47709e0372e3d56f78
ohveloper <ohveloper@gmail.com>
2021-04-13 16:32:22 +0900

    axios all done

---
+++ src/api/delUserWithdrawal.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const api = process.env.REACT_APP_SERVER_ADDRESS || 'https://localhost:5000';
+
+const apiClient = axios.create({
+  baseURL: api,
+  responseType: 'json',
+  headers: {
+    'Content-Type': 'application/json',
+    'withCredentials': true,
+    'authorization':
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjE4Mjg4ODM0LCJleHAiOjE2MTgzMDY4MzR9.t03w_OsLusXuBoZZn2j7OLWi6dfWPrGGem6RkQx7Wkg',
+  },
+});
+
+export async function delUserWithdrawalT() {
+  const response = await apiClient.delete('/user');
+  return response.data;
+}
