9f61b1aab9e8d8e8279346879542d503038ed411
riley909 <increaseblue666@gmail.com>
2021-04-15 14:57:46 +0900

    resetting actions and reducer

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -4,9 +4,7 @@ export const GET_RANK = 'GET_RANK' as const;
 export const GET_COMMENTS = 'GET_COMMENTS' as const;
 export const GET_USER_FEEDS = 'GET_USER_FEEDS' as const;
 
-import { AxiosError } from 'axios';
 import { Dispatch } from 'redux';
-import { createAsyncAction } from 'typesafe-actions';
 import { postBringUserInfoT, UUID } from '../api/postBringUserInfo';
 import { postBringFeedT } from '../api/postBringFeeds';
 import { FeedId, postLikeFeedT } from '../api/postLikeFeed';
@@ -20,95 +18,21 @@ import {
   UserFeed,
 } from '../reducers/initialState';
 import { getRankT } from '../api/getRank';
-import { BringComment, PrivateFeedT, Welcome } from '../reducers/reducer';
-import { UserInfoT } from '../reducers/reducer';
-import { FeedLike } from '../api/postLikeFeed';
-import { Content, postUploadFeedT, UploadFeed } from '../api/postUploadFeed';
+import { Content, postUploadFeedT } from '../api/postUploadFeed';
 import { postBringCommentT } from '../api/postBringComment';
-import { AuthCode, postSignUpT, SignUp } from '../api/postSignUp';
-import { AccessToken, postLoginT } from '../api/postLogin';
-import { FeedIdLimitUserId, postGetUserFeedsT } from '../api/postGetUserFeeds';
+import { AuthCode, postSignUpT } from '../api/postSignUp';
+import { postLoginT } from '../api/postLogin';
 
 import {
-  POST_LOG_IN_API,
-  POST_LOG_IN_ERROR,
-  POST_LOG_IN_SUCCESS,
-  POST_SIGN_UP_API,
-  POST_SIGN_UP_ERROR,
-  POST_SIGN_UP_SUCCESS,
-  POST_BRING_USER_INFO_API,
-  POST_BRING_USER_INFO_ERROR,
-  POST_BRING_USER_INFO_SUCCESS,
-  POST_UPLOAD_FEED_SUCCESS,
-  POST_UPLOAD_FEED_API,
-  POST_UPLOAD_FEED_ERROR,
-  POST_BRING_FEEDS_API,
-  POST_BRING_FEEDS_SUCCESS,
-  POST_BRING_FEEDS_ERROR,
-  PATCH_EDIT_FEED_API,
-  PATCH_EDIT_FEED_ERROR,
-  PATCH_EDIT_FEED_SUCCESS,
-  DELETE_REMOVE_FEED_API,
-  DELETE_REMOVE_FEED_SUCCESS,
-  DELETE_REMOVE_FEED_ERROR,
-  GET_RANK_API,
-  GET_RANK_ERROR,
-  GET_RANK_SUCCESS,
-  POST_LIKE_FEED_API,
-  POST_LIKE_FEED_ERROR,
-  POST_LIKE_FEED_SUCCESS,
-  POST_BRING_COMMENT_API,
-  POST_BRING_COMMENT_SUCCESS,
-  POST_BRING_COMMENT_ERROR,
+  postLogInAsync,
+  postBringCommentAsync,
+  postSignUpAsync,
+  postLikeFeedAsync,
+  postUploadFeedsAsync,
+  postBringFeedsAsync,
+  postBringUserInfoAsync,
+  getRankAsync,
 } from '../actions/actionTypes';
-
-export const postLogInAsync = createAsyncAction(
-  POST_LOG_IN_API,
-  POST_LOG_IN_SUCCESS,
-  POST_LOG_IN_ERROR
-)<undefined, AccessToken, AxiosError>();
-
-export const postSignUpAsync = createAsyncAction(
-  POST_SIGN_UP_API,
-  POST_SIGN_UP_SUCCESS,
-  POST_SIGN_UP_ERROR
-)<undefined, SignUp, AxiosError>();
-
-export const postBringCommentAsync = createAsyncAction(
-  POST_BRING_COMMENT_API,
-  POST_BRING_COMMENT_SUCCESS,
-  POST_BRING_COMMENT_ERROR
-)<undefined, BringComment, AxiosError>();
-
-export const postBringFeedsAsync = createAsyncAction(
-  POST_BRING_FEEDS_API,
-  POST_BRING_FEEDS_SUCCESS,
-  POST_BRING_FEEDS_ERROR
-)<undefined, Welcome, AxiosError>();
-
-export const postUploadFeedsAsync = createAsyncAction(
-  POST_UPLOAD_FEED_API,
-  POST_UPLOAD_FEED_SUCCESS,
-  POST_UPLOAD_FEED_ERROR
-)<undefined, UploadFeed, AxiosError>();
-
-export const postBringUserInfoAsync = createAsyncAction(
-  POST_BRING_USER_INFO_API,
-  POST_BRING_USER_INFO_SUCCESS,
-  POST_BRING_USER_INFO_ERROR
-)<undefined, UserInfoT, AxiosError>();
-
-export const getRankAsync = createAsyncAction(
-  GET_RANK_API,
-  GET_RANK_SUCCESS,
-  GET_RANK_ERROR
-)<undefined, Rank, AxiosError>();
-
-export const postLikeFeedAsync = createAsyncAction(
-  POST_LIKE_FEED_API,
-  POST_LIKE_FEED_SUCCESS,
-  POST_LIKE_FEED_ERROR
-)<undefined, FeedLike, AxiosError>();
 
 export function postLogInThunk(authCode: AuthCode) {
   return async (dispatch: Dispatch) => {
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,15 +1,12 @@
 import { combineReducers } from 'redux';
-import poemReducer, { poemInitState } from './poemReducer';
 import reducer from './reducer';
 import { InitState } from './reducer';
 
 const rootReducer = combineReducers({
-  poemReducer,
   reducer,
 });
 
 export default rootReducer;
 export type RootState = {
-  poemReducer: poemInitState;
   reducer: InitState;
 };
