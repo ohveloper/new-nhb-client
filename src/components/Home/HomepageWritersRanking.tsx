089a3c175105ba234fec0cd31aa5fbdcb034d772
Sentence <perfectsentence@Sentenceui-iMac.local>
2021-04-18 03:56:17 +0900

    Feature#1 : add UserLiv e Ranking

--- src/components/Home/HomepageWritersRanking.tsx
+++ src/components/Home/HomepageWritersRanking.tsx
@@ -3,9 +3,8 @@ import { getUserLiveRankingT } from '../../api/getUserLiveRanking';
 import { useDispatch } from 'react-redux';
 
 export default function HomepageWritersRanking() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const dispatch = useDispatch();
+  dispatch(getUserLiveRankingT());
+
+  return 
 }
