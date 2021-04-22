61508674868364eedd2138e3c70976165b462b8b
ohveloper <ohveloper@gmail.com>
2021-04-23 04:32:46 +0900

    Feat#214 mypage badge select function done

--- src/components/myPage/modals/Badges_Modal.tsx
+++ src/components/myPage/modals/Badges_Modal.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import './Badges_Modal.css';
+import React, { MouseEventHandler, useEffect, useRef } from 'react';
+import './Badges_Modal.scss';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../reducers';
@@ -18,15 +18,22 @@ export default function Badges_Modal({ badgeModalHandler }: Badges_ModalType) {
     console.log(state);
   }, [state]);
 
+  console.log('userInfo:', state.userInfo.data?.data.userInfo);
   //! 요청보낼 tagId 저장소
   const [value, setValue] = useState('');
 
+  //! 모든 테그의 정보
+  const allTags = state.tags.data?.data;
+
   //! 내가 가지고 있는 tag의 정보
   const myTagsInfo = state.userInfo.data?.data.userInfo.tags;
+  const _myTagsId = myTagsInfo?.map((x) => x.tagId);
+  console.log(myTagsInfo);
 
   //! 내가 선택한 테그 정보 (아직 요청 보내기 전)
   const pickHandler = (e: any) => {
     setValue(e.target.textContent);
+    // e.target.previousSibling.checked = !e.target.previousSibling.checked;
   };
 
   //! 내가 선택하거나, 선택한걸 뺴고싶은 요청
@@ -35,6 +42,7 @@ export default function Badges_Modal({ badgeModalHandler }: Badges_ModalType) {
       return;
     }
     const tagId = Number(value.split(',')[0]);
+    console.log('tagId:', tagId);
 
     const _accessToken = '';
     if (state.accessToken) {
@@ -57,42 +65,46 @@ export default function Badges_Modal({ badgeModalHandler }: Badges_ModalType) {
         <button onClick={setUpBadgeHandler}>설정완료</button>
         <button onClick={badgeModalHandler}>닫기</button>
         <div>
-          <div>
-            <h3>내가 가지고있는 테그들</h3>
-            <div style={{ color: 'red' }}>
-              {
-                //? 가지고 있는 테그들 렌더
-                myTagsInfo?.map((badge) =>
-                  //? 선택했는지 확인
-                  !badge.isUsed ? (
-                    //? false일 확률이 더 높으니까 앞에
-                    //? 내가 선택하지 않은 테그 : 빨강글씨 테두리 없음
-                    <>
-                      <div key={badge.tagId} style={{ cursor: 'pointer' }}>
-                        <div onClick={pickHandler}>
-                          {badge.tagId},{badge.isUsed},{badge.tagName}
-                        </div>
-                        <div style={{ display: 'none' }}>
-                          {badge.description}
-                        </div>
-                      </div>
-                    </>
-                  ) : (
-                    //? 내가 선택한 테그 :테두리 파랑
+          <h3>내가 가지고있는 테그들</h3>
+          <div style={{ color: 'red' }}>
+            {
+              //? 가지고 있는 테그들 렌더
+              myTagsInfo?.map((badge) =>
+                //? 선택했는지 확인
+                !badge.isUsed ? (
+                  //? false일 확률이 더 높으니까 앞에
+                  //? 내가 선택하지 않은 테그 : 빨강글씨 테두리 없음
+                  <>
                     <div
                       key={badge.tagId}
                       style={{ border: '2px solid blue', cursor: 'pointer' }}
                       onClick={pickHandler}
                     >
-                      <div>
-                        {badge.tagId},{badge.isUsed},{badge.tagName},
-                        {badge.description}
-                      </div>
+                      <label>
+                        <input type="radio" name="chk_badge" />
+                        <div id={'tag-id-'.concat(String(badge.tagId))}>
+                          {badge.tagId},{badge.isUsed},{badge.tagName}
+                        </div>
+                      </label>
                     </div>
-                  )
+                  </>
+                ) : (
+                  //? 내가 선택한 테그 :테두리 파랑
+                  <div
+                    key={badge.tagId}
+                    style={{ border: '2px solid blue', cursor: 'pointer' }}
+                    onClick={pickHandler}
+                  >
+                    <label>
+                      <input type="radio" name="chk_badge" />
+                      <div id={'tag-id-'.concat(String(badge.tagId))}>
+                        {badge.tagId},{badge.isUsed},{badge.tagName},
+                      </div>
+                    </label>
+                  </div>
                 )
-              }
-            </div>
+              )
+            }
           </div>
         </div>
       </div>
